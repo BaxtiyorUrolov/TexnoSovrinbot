16bd6c20d18ff9f5824bd5b1cd54a5007fc678bb
Baxtiyor Urolov <uralov2908@gmail.com>
2025-02-04 14:12:54 +0500

    same changes

--- cmd/main.go
+++ cmd/main.go
@@ -47,11 +47,15 @@ func main() {
 	router := gin.Default()
 
 	// 🔹 CORS middleware qo'shish
-	router.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		if c.Request.Method == "OPTIONS" {
+	router.Use(func(c *gin.Context) { // ✅ To'g'ri funksiya sintaksisi
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Methods", "POST, GET")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Max-Age", "3600")
+		c.Header("Content-Type", "application/json")
+
+		if c.Request.Method == http.MethodOptions {
 			c.AbortWithStatus(http.StatusNoContent)
 			return
 		}
